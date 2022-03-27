b067a2a60e31faf87bc1bec6fc16b52dbdc12ee1
Jesper L. Nielsen <lyager@duck.com>
2022-03-27 12:32:28 +0200

    Added logging to file from fetch.py

--- spotprice/fetch.py
+++ spotprice/fetch.py
@@ -18,12 +18,29 @@ def get_json():
 
 def to_backend(json, backend: Backend):
     for i in json['result']['records']:
-        backend.add_price(i['_id'], i['HourUTC'], i['HourDK'], i['PriceArea'], i['SpotPriceDKK'], i['SpotPriceEUR'])
+        backend.add_price(i['_id'], i['HourUTC'], i['HourDK'],
+                          i['PriceArea'], i['SpotPriceDKK'], i['SpotPriceEUR'])
 
 
 if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Fetch latest Energy prices")
+    parser.add_argument("--logging", help="Logging location", action="store",
+                        type=str)
+    args = parser.parse_args()
+
+    if args.logging:
+        # Setup logging to file
+        logging.basicConfig(filename="{}/spotprice.log".format(args.logging),
+                            filemode='a',
+                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                            datefmt='%H:%M:%S',
+                            level=logging.INFO)
+    else:
+        logging.getLogger().setLevel(logging.INFO)
+
     j = get_json()
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.INFO)
     b = Backend(logging)
     to_backend(j, b)
+    del b
